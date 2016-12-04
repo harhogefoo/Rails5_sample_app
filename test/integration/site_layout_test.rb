452eb9fab922629676887ab5d0bdcf6e7430eff8
harhogefoo <harimanmon@gmail.com>
2016-12-04 16:58:56 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -13,6 +17,26 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_template 'users/new'
     assert_select "title", full_title("Sign up")
+
+    # ログインしていない状態のアクセス
+    get users_path
+    follow_redirect!
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+
+    # ログイン状態のアクセス
+    log_in_as(@user)
+    get users_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
   end
+
+
 
 end
